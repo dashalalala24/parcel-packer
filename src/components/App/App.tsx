3964965baef7fe67cb1c1141872e2d2de59747ad
Daria Lazarchuk <dashal_O_o@mail.ru>
2023-06-09 23:14:08 +0300

    refactor: counter refactor

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className='page'>
       <Counter
-        quantity={4}
-        percentage={100}
-        // percentage – это сколько процентов товаров отсканировано: 0 - не отсканирован ни один товар, 100 - все отсканированы
+        itemsScanned={0}
+        itemsTotal={10}
       />
     </div>
   );
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,25 +2,34 @@ import { FC } from 'react';
 import './Counter.css';
 
 interface ICounter {
-  quantity: number;
-  percentage: number;
+  itemsTotal: number;
+  itemsScanned: number;
 }
 
-const CounterStyle = (percentage: number) => {
-  return {
-    // background: `linear-gradient(to right, rgba(42, 173, 46, ${
-    //   percentage === 100 ? 1 : (100 - percentage) / 100
-    // }) 50%, var(--beige-color) 50%)`,
-    backgroundPosition: `${100 - percentage}%`,
+const Counter: FC<ICounter> = ({ itemsTotal, itemsScanned }) => {
+  const counterClassName = (
+    className: string,
+    itemsTotal: number,
+    itemsScanned: number
+  ): string => {
+    return itemsTotal === itemsScanned ? `${className} ${className}_full` : className;
   };
-};
 
-const Counter: FC<ICounter> = ({ quantity, percentage }) => {
+  const counterStyle =
+    itemsTotal === itemsScanned
+      ? undefined
+      : { backgroundPosition: `${100 - (itemsScanned / itemsTotal) * 100}%` };
+
+  const counterText: string =
+    itemsScanned > 0 && itemsScanned < itemsTotal
+      ? `${itemsScanned}/${itemsTotal}`
+      : `${itemsTotal} шт.`;
+
   return (
     <div
-      className='counter'
-      style={CounterStyle(percentage)}>
-      <p className={`counter__text ${percentage ? 'counter__text_full' : ''}`}>{quantity} шт.</p>
+      className={counterClassName('counter', itemsTotal, itemsScanned)}
+      style={counterStyle}>
+      <p className={counterClassName('counter__text', itemsTotal, itemsScanned)}>{counterText}</p>
     </div>
   );
 };

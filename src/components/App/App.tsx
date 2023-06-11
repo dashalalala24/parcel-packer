77ee7cc9b4377d56bf5b90622b69c58d1db6469b
Daria Lazarchuk <dashal_O_o@mail.ru>
2023-06-11 23:24:18 +0300

    add Navbar component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,16 @@
-import "./App.css";
+import './App.css';
 import Tag from '../Tag/Tag';
-import Counter from "../Counter/Counter";
-import Icon from "../Icon/Icon";
-import Button, { ButtonColors, ButtonSizes } from "../Button/Button";
-import { IconImages } from "../Icon/types";
-import ActionButton from "../ActionButton/ActionButton";
+import Counter from '../Counter/Counter';
+import Icon from '../Icon/Icon';
+import Button, { ButtonColors, ButtonSizes } from '../Button/Button';
+import { IconImages } from '../Icon/types';
+import ActionButton from '../ActionButton/ActionButton';
+import Navbar, { navbarStatuses } from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
 function App() {
   return (
-    <div className="page">
+    <div className='page'>
       <br />
       <Counter
         itemsScanned={0}
@@ -21,69 +22,83 @@ function App() {
         // value='5 товаров'
       />
       <br />
-      <Tag
-        type='YMA'
-      />
+      <Tag type='YMA' />
       {/* Для проверки всех видов тегов: 'YMA' - коробка, 'MYA' - пакет, 'NONPACK' - другие упаковки,
       1234567823432 - штриход, 'IMEI', 'chestnyy_znak', 'cancel', 'info'. 
       value только для 'info' */}
       <br />
-      <Counter itemsScanned={3} itemsTotal={5} />
+      <Counter
+        itemsScanned={3}
+        itemsTotal={5}
+      />
       <br />
-      <Icon imgName={IconImages.burger} width={70} />
+      <Icon
+        imgName={IconImages.burger}
+        width={70}
+      />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         size={ButtonSizes.xs}
-        text="Изменить состав"
+        text='Изменить состав'
         icon={IconImages.edit}
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         size={ButtonSizes.s}
-        text="Ввести с клавиатуры"
+        text='Ввести с клавиатуры'
         icon={IconImages.keyboard}
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         color={ButtonColors.black}
         size={ButtonSizes.m}
-        text="Назад"
+        text='Назад'
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         color={ButtonColors.beige}
         size={ButtonSizes.l}
-        text="Позвать бригадира"
+        text='Позвать бригадира'
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         color={ButtonColors.yellow}
         size={ButtonSizes.xl}
-        text="Готово"
+        text='Готово'
         icon={IconImages.qrCode}
       />
       <br />
-      <ActionButton icon={IconImages.cross} onClick={() => {console.log("Hello")}} />
+      <ActionButton
+        icon={IconImages.cross}
+        onClick={() => {
+          console.log('Hello');
+        }}
+      />
       <br />
-      <ActionButton icon={IconImages.backArrow} onClick={() => {console.log("Hello")}} />
-
+      <ActionButton
+        icon={IconImages.backArrow}
+        onClick={() => {
+          console.log('Hello');
+        }}
+      />
+      <br />
+      <Navbar status={navbarStatuses.default} />
       <Footer />
-
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,62 @@
+import { CSSProperties, FC } from 'react';
+import './Navbar.css';
+import Button, { ButtonColors, ButtonSizes } from '../Button/Button';
+import IconImages from '../Icon/types';
+
+export enum navbarStatuses {
+  default = 'default',
+  success = 'success',
+  error = 'error',
+  anomaly = 'anomaly',
+}
+
+const navbarStyle = (status: navbarStatuses): CSSProperties => ({
+  backgroundColor:
+    status === navbarStatuses.success
+      ? 'var(--green-color)'
+      : status === navbarStatuses.error
+      ? 'var(--red-color)'
+      : status === navbarStatuses.anomaly
+      ? 'var(--orange-color)'
+      : 'var(--white-color)',
+});
+
+interface INavbar {
+  status: navbarStatuses;
+}
+const Navbar: FC<INavbar> = ({ status }) => {
+  return (
+    <nav
+      className='navbar'
+      style={navbarStyle(status)}>
+      <Button
+        onClick={() => {
+          console.log('Hello');
+        }}
+        color={status === 'default' ? ButtonColors.black : ButtonColors.white}
+        size={ButtonSizes.m}
+        text='Назад'
+      />
+      <Button
+        onClick={() => {
+          console.log('Hello');
+        }}
+        size={ButtonSizes.s}
+        color={status === 'default' ? ButtonColors.white : ButtonColors.transparent}
+        text='Ввести с клавиатуры'
+        icon={IconImages.keyboard}
+      />
+      <Button
+        onClick={() => {
+          console.log('Hello');
+        }}
+        size={ButtonSizes.xs}
+        color={status === 'default' ? ButtonColors.white : ButtonColors.transparent}
+        text='Изменить состав'
+        icon={IconImages.edit}
+      />
+    </nav>
+  );
+};
+
+export default Navbar;

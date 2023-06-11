8fb3f6fc2220bebfa8d6feb1d554b754b1b3b993
Igor Topal <i@itopal.ru>
2023-06-12 01:46:09 +0300

    add notification component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,27 @@
-import "./App.css";
+import './App.css';
 import Tag from '../Tag/Tag';
-import Counter from "../Counter/Counter";
-import Icon from "../Icon/Icon";
-import Button, { ButtonColors, ButtonSizes } from "../Button/Button";
-import { IconImages } from "../Icon/types";
-import ActionButton from "../ActionButton/ActionButton";
+import Counter from '../Counter/Counter';
+import Icon from '../Icon/Icon';
+import Button, { ButtonColors, ButtonSizes } from '../Button/Button';
+import { IconImages } from '../Icon/types';
+import ActionButton, {
+  ActionButtonBackground,
+} from '../ActionButton/ActionButton';
 import Footer from '../Footer/Footer';
+import Notification, { NotificationType } from '../Notification/Notification';
 
 function App() {
   return (
-    <div className="page">
+    <div className='page'>
       <br />
-      <Counter
-        itemsScanned={0}
-        itemsTotal={2}
-      />
+      <Counter itemsScanned={0} itemsTotal={2} />
       <br />
       <Tag
         type='YMA'
         // value='5 товаров'
       />
       <br />
-      <Tag
-        type='YMA'
-      />
+      <Tag type='YMA' />
       {/* Для проверки всех видов тегов: 'YMA' - коробка, 'MYA' - пакет, 'NONPACK' - другие упаковки,
       1234567823432 - штриход, 'IMEI', 'chestnyy_znak', 'cancel', 'info'. 
       value только для 'info' */}
@@ -34,56 +32,94 @@ function App() {
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         size={ButtonSizes.xs}
-        text="Изменить состав"
+        text='Изменить состав'
         icon={IconImages.edit}
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         size={ButtonSizes.s}
-        text="Ввести с клавиатуры"
+        text='Ввести с клавиатуры'
         icon={IconImages.keyboard}
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         color={ButtonColors.black}
         size={ButtonSizes.m}
-        text="Назад"
+        text='Назад'
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         color={ButtonColors.beige}
         size={ButtonSizes.l}
-        text="Позвать бригадира"
+        text='Позвать бригадира'
       />
       <br />
       <Button
         onClick={() => {
-          console.log("Hello");
+          console.log('Hello');
         }}
         color={ButtonColors.yellow}
         size={ButtonSizes.xl}
-        text="Готово"
+        text='Готово'
         icon={IconImages.qrCode}
       />
       <br />
-      <ActionButton icon={IconImages.cross} onClick={() => {console.log("Hello")}} />
+      <ActionButton
+        icon={IconImages.cross}
+        iconColor='black'
+        backgroundColor={ActionButtonBackground.transparent}
+        onClick={() => {
+          console.log('Hello');
+        }}
+      />
       <br />
-      <ActionButton icon={IconImages.backArrow} onClick={() => {console.log("Hello")}} />
+      <ActionButton
+        icon={IconImages.backArrow}
+        backgroundColor={ActionButtonBackground.beige}
+        iconColor='white'
+        onClick={() => {
+          console.log('Hello');
+        }}
+      />
+      <br />
+      <Notification
+        message='Бригадир скоро подойдёт'
+        type={NotificationType.info}
+      />
+      <br />
+      <Notification
+        message='Штрихкод скопирован'
+        type={NotificationType.success}
+      />
+      <br />
+      <Notification message='Товар не найден' type={NotificationType.fault} />
+      <br />
+      <Notification
+        message='Сканируйте IMEI товара'
+        child={<p>ТУТ БУДЕТ КОМПОНЕНТ АЙТЕМА</p>}
+        type={NotificationType.warning}
+      />
+      <br />
+      <Notification
+        message='Ошибка 505'
+        messageDetails='Сервер не смог обработать полученный запрос'
+        type={NotificationType.systemError}
+      />
+      <br />
 
       <Footer />
-
     </div>
   );
 }
--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -1,16 +1,21 @@
 export enum IconImages {
   backArrow = "backArrow",
+  bag = "bag",
   barcode = "barcode",
+  box = "box",
   burger = "burger",
+  cancel = "cancel",
   cross = "cross",
   edit = "edit",
+  error = "error",
+  info = "info",
   kebab = "kebab",
   keyboard = "keyboard",
-  market = "market",
   qrCode = "qrCode",
   rocket = "rocket",
-  warning = "warning",
-  yaLogo = "yaLogo",
+  success = "success",
+  warningFilled = "warningFilled",
+  warningOutline = "warningOutline",
 }
 
 export default IconImages;

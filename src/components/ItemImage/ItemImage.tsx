a2dc9167c19187fae63bea30112702c54c661095
Daria Lazarchuk <dashal_O_o@mail.ru>
2023-06-13 16:55:37 +0300

    refactor: ItemCard, Tag & ItemImage

--- src/components/ItemImage/ItemImage.tsx
+++ src/components/ItemImage/ItemImage.tsx
@@ -5,18 +5,13 @@ interface IItemImage {
   itemImg: string;
   itemName: string;
   itemQuantity?: number;
-  deleted?: boolean;
 }
 
-const ItemImage: FC<IItemImage> = ({ itemImg, itemName, itemQuantity, deleted = false }) => {
+const ItemImage: FC<IItemImage> = ({ itemImg, itemName, itemQuantity = 1 }) => {
   return (
     <div className='item-image'>
-      <img
-        className={deleted ? 'item-image__pic item-image__pic_deleted' : 'item-image__pic'}
-        alt={itemName}
-        src={itemImg}
-      />
-      {itemQuantity ? <span className='item-image__label'>{itemQuantity}</span> : null}
+      <img className='item-image__pic' alt={itemName} src={itemImg} />
+      {itemQuantity > 1 ? <span className='item-image__label'>{itemQuantity}</span> : null}
     </div>
   );
 };
---
+++ src/utils/constants.tsx
@@ -0,0 +1,32 @@
+interface IPacks {
+  box: string[];
+  bag: string[];
+  other: string[];
+}
+
+export const packNames: IPacks = {
+  box: [
+    'YMA',
+    'YMC',
+    'YME',
+    'YMF',
+    'YMG',
+    'YMH',
+    'YMJ',
+    'YML',
+    'YMN',
+    'YMO',
+    'YMP',
+    'YMQ',
+    'YMP',
+    'YMR',
+    'YMS',
+    'YMU',
+    'YMV',
+    'YMW',
+    'YMX',
+    'YMY',
+  ],
+  bag: ['MYA', 'MYB', 'MYC', 'MYD', 'MYE'],
+  other: ['KSD', 'NONPACK', 'STRETCH'],
+};

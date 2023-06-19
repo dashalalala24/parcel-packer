ee4094df5f941766388dd2d59c02c952388018bb
Igor Topal <i@itopal.ru>
2023-06-19 12:00:42 +0300

    hotfix

--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import exampleReducer from './slices/example/example';
 import notificationReducer from './slices/notification/notification';
-import orderReducer from './slices/order/order';
 import asyncDispatchMiddleware from './middleware/asyncDispatchMiddleware';
 
 export const store = configureStore({
   reducer: {
     example: exampleReducer,
     notification: notificationReducer,
-    order: orderReducer,
   },
   middleware: gDM => gDM().concat(asyncDispatchMiddleware),
 });
